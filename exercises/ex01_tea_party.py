b7504bca61f1aac657cbea54b89c0b2de85db3cf
Aattaway7 <attaway@unc.edu>
2024-09-22 15:05:14 -0400

    Progress on Exercise 1

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -5,11 +5,17 @@ __author__: str = "730810493"
 
 def main_planner(guests: int) -> None:
     """brings all the subsequent functions together to print intended values"""
-    print("A Cozy Tea Party for " + str(guests) + " People!")
+    print(
+        "A Cozy Tea Party for " + str(guests) + " People!"
+    )  # prints the standard message with constimized number of guests
 
-    print("Tea Bags: " + str(tea_bags(people=guests)))
+    print(
+        "Tea Bags: " + str(tea_bags(people=guests))
+    )  # calls the tea_bags function to display number of bags needed concatinated with "Tea Bags"
 
-    print("Treats: " + str(treats(people=guests)))
+    print(
+        "Treats: " + str(treats(people=guests))
+    )  # calls the treats function to display the number of treats needed concatinated with "Treats"
 
     print(
         "Cost: $"
@@ -19,14 +25,24 @@ def main_planner(guests: int) -> None:
     )
 
 
+# calls the tea_count function and treat_count function as arguements for cost
+
+
 def tea_bags(people: int) -> int:
     """calculates the number of tea bags that are needed"""
     return people * 2
 
 
+# mutiplies the number of people by 2 to obtain the number of bags needed and
+# is stored as the return value
+
+
 def treats(people: int) -> int:
     """calculates the number of treats needed for the party"""
-    return int(tea_bags(people=people) * 1.5)
+    return int(
+        tea_bags(people=people) * 1.5
+    )  # calls the tea_bag function and multipies it by 1.5 to obtain the
+    # amount of treats needed
 
 
 def cost(tea_count: int, treat_count: int) -> float:
@@ -34,5 +50,10 @@ def cost(tea_count: int, treat_count: int) -> float:
     return (tea_count * 0.5) + (treat_count * 0.75)
 
 
+# calls tea_count and treat_count to muitply them by .5 and .75 respectivly
+# it then adds the two products together
+
+
 if __name__ == "__main__":
     main_planner(guests=int(input("How many guests are attending your tea party?")))
+# needed conditional statement to execute with autograder
